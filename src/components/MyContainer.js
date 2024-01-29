93a1619f71e5262db680b2e675cffc8ae678b3f0
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2024-01-29 14:35:39 +0200

    a

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -3,12 +3,39 @@ import React from 'react';
 
 
 function Container() {
-    return <container><MyList
-        header="Really epic list component"
-        items={[
-            { id: "1", text: "This is an item" },
-            { id: "2", text: "Also this"},
-        ]}
+    const [items, setItems] = useState([
+        { id: "1", text: "This is an item" },
+        { id: "2", text: "Also this"},
+    ]);
+
+    const [newItemText, setNewItemText] = useState('')
+
+    const onClick = (e) => {
+        e.preventDevault();
+        if (newItemText.trim() != '') {
+            const newItem = {
+                id: (items.length + 1).toString(),
+                text: newItemText.trim()
+            };
+            setItems([...items, newItem]);
+            setNewItemText('');
+        };
+    };
+
+    const textAreaChange = (e) => {
+        setNewItemText(e.target.value);
+    }
+
+    return <container>
+        <textarea
+            value={newItemText}
+            onChange={textAreaChange}
+            placeholder="Enter new item text"
+        />
+        <button type="submit" onClick={onClick}></button>
+        <MyList
+                header="Really epic list component"
+                items={items}
         />
         </container>
 }
