c807bb4f4478a6c99213e37dd66d34c97eb6ce07
Xijaxa <81866800+Xijaxa@users.noreply.github.com>
2024-01-29 13:44:45 +0200

    a

--- src/components/MyContainer.js
+++ src/components/MyContainer.js
@@ -2,7 +2,14 @@ import MyList from './MyList'
 
 
 function Container(props) {
-    return <container><MyList></MyList></container>
+    return <container><MyList
+        header="Really epic list component"
+        items={[
+            { id: "1", text: "This is an item" },
+            { id: "2", text: "Also this"},
+        ]}
+        />
+        </container>
 }
 
 export default Container;
